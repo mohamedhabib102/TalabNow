5234f2d1ff6f55217b9ff1bc0b6a3729595414d1
Mohamed Habib <byaalkhy43@gmail.com>
2025-12-26 00:25:52 +0200

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import Logo from "./logo";
+import Logo from "./Logo";
 
 export default function Footer() {
     const { t } = useTranslation();
